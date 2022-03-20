37194d8aa0e90375e87e0e43d19adfeded9b5288
funXMutulay <mkebe1@yhoo.fr>
2022-03-19 18:57:01 -0700

    commande cloud push

---
+++ java/org/nioun/essentials/commande/config/ConfigClass.java
@@ -0,0 +1,16 @@
+package org.nioun.essentials.commande.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class ConfigClass {
+
+	@Bean
+    public JavaMailSender javaMailSender() { 
+          return new JavaMailSenderImpl();
+    }
+	
+}
